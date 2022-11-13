e37f47cad24793c346b8b3fcce547ea449861175
SORO Dognimani <dognimanisoro@gmail.com>
2022-11-13 21:55:14 +0100

    finalisation fichier model.py avec la mise en relation des différentes table

--- Back-end/EnvironnementVirtuel/app/model.py
+++ Back-end/EnvironnementVirtuel/app/model.py
@@ -1,51 +1,58 @@
-from sqlalchemy import column, Integer, String,Float,ForeignKey
-from config import Base
+from sqlalchemy import Column, Integer, String,Float,ForeignKey,DateTime, Boolean, Table
+from config import Base, engine
 from sqlalchemy.orm import relationship
+import datetime
 # la table molecule
 
 class Molecule(Base):
-    __tablename__ = 'molecule'
-    CAS_Number=column(String,primary_key=True)
-    Name=column(String)
-    Canonical_Formula=column(String)
-    Molecular_Weight=column(Float)
-    XLogP3=column(Float)
-    HydrogenBondDonorCount=column(Float)
-    HydrogenBondAcceptorCount=column(Float)
-    Rotatable_Bond_Count=column(Float)
-    ExactMass=column(Float)
-    MonoisotopicMass=column(Float)
-    TopologicalPolarSurfaceArea=column(Float)
-    Heavy_Atom_Count=column(Float)
-    Formal_Charge=column(Float)
-    Complexity=column(Float)
-    Isotope_Atom_Count=column(Float)
-    Undefined_Atom_Stereocenter_Count=column(Float)
-    Defined_Bond_Stereocenter_Count=column(Float)
-    Undefined_Bond_Stereocenter_Count=column(Float)
-    Covalently_Bonded_Unit_Count=column(Float)
-    Compound_Is_Canonicalized=column(Float)
-    Results = relationship("result",back_populates="Molecules") # mise en place de la clé étrnagère : result= nom de la table enfant et Molecules = le nom de la clé étrangère dans dans la table result
+    __tablename__ = 'molecules'
+    CAS_Number=Column(String,primary_key=True)
+    Name=Column(String)
+    Canonical_Formula=Column(String)
+    Molecular_Weight=Column(Float)
+    XLogP3=Column(Float)
+    HydrogenBondDonorCount=Column(Float)
+    HydrogenBondAcceptorCount=Column(Float)
+    Rotatable_Bond_Count=Column(Float)
+    ExactMass=Column(Float)
+    MonoisotopicMass=Column(Float)
+    TopologicalPolarSurfaceArea=Column(Float)
+    Heavy_Atom_Count=Column(Float)
+    Formal_Charge=Column(Float)
+    Complexity=Column(Float)
+    Isotope_Atom_Count=Column(Float)
+    Undefined_Atom_Stereocenter_Count=Column(Float)
+    Defined_Bond_Stereocenter_Count=Column(Float)
+    Undefined_Bond_Stereocenter_Count=Column(Float)
+    Covalently_Bonded_Unit_Count=Column(Float)
+    Compound_Is_Canonicalized=Column(Boolean, default=True)
+    Created_date=Column(DateTime, default= datetime.datetime.utcnow)
+    results = relationship("Result",back_populates="molecules") # mise en place de la clé étrnagère dans la table result : result= nom de la table enfant et Molecules = le nom de la clé étrangère dans dans la table result
 
 # la table Result
 class Result(Base):
-    __tablename__ ='result'
-    id=column(Integer,primary_key=True)
-    Toxicity_Type=column(String)
-    value=column(String) # pas sûr que ce soit string mais plutôt text
-    CAS_Number=column(String,ForeignKey("molecule.Numero_CAS")) # à revoir poour la partie clé étrangère
-    Molecules = relationship("molecule",back_populates="Results") # mapping de la clé étrangère venant de la table molécule
+    __tablename__ ='results'
+    id=Column(Integer,primary_key=True)
+    Toxicity_Type=Column(String)
+    value=Column(String) # pas sûr que ce soit string mais plutôt text
+    CAS_Number=Column(String,ForeignKey("molecules.Numero_CAS")) #  la clé étrangère
+    molecules = relationship("Molecule",back_populates="results") # mapping de la clé étrangère venant de la table molécule
+    references=relationship("Reference" , secondary="referencements", back_populates="results")
 
 
 # table Reference
 class Reference(Base):
-    __tablename__ ='reference'
-    id=column(Integer,primary_key=True)
-    number=column(Integer)
-    value=column(String) # pas sûr que ce soit string mais plutôt text
+    __tablename__ ='references'
+    id=Column(Integer,primary_key=True)
+    number=Column(Integer)
+    value=Column(String) # pas sûr que ce soit string mais plutôt text
+    results=relationship("Result", secondary="referencements", back_populates="references")
 
 # table Référencement
-class Referencement(Base):
-    __tablename__ ='referencement'
-    result_id=column(Integer)
-    reference_id=column(Integer)
+
+referencements = Table('referencements', Base.metadata,
+    Column('result_id', ForeignKey('resultats.id'), primary_key=True),
+    Column('reference_id', ForeignKey('references.id'), primary_key=True)
+)
+# Create the tables in the database
+#Base.metadata.create_all(engine)
